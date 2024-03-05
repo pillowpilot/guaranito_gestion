84d0f4e22bc2b2f7419ca69fccb8e70633c17b67
federico <carlos.federico005@gmail.com>
2024-03-05 16:30:28 -0300

    feat (accounts): add view, add, and change permissions to company_user and company_manager groups

--- backend/accounts/management/commands/create_default_groups.py
+++ backend/accounts/management/commands/create_default_groups.py
@@ -10,10 +10,17 @@ class Command(BaseCommand):
         company_manager, created = Group.objects.get_or_create(name="company_manager")
 
         user_related_codenames = ["view_user", "add_user", "change_user"]
+        parcel_related_codenames = ["view_parcel", "add_parcel", "change_parcel"]
         try:
             for codename in user_related_codenames:
                 perm = Permission.objects.get(codename=codename)
                 company_manager.permissions.add(perm)
+                
+            for codename in parcel_related_codenames:
+                perm = Permission.objects.get(codename=codename)
+                company_manager.permissions.add(perm)
+                company_user.permissions.add(perm)
+
         except Permission.DoesNotExist:
             self.stderr.write(f"Permission with codename {codename} does not exists")
         return None
--- backend/accounts/tests/test_create_default_groups.py
+++ backend/accounts/tests/test_create_default_groups.py
@@ -43,3 +43,21 @@ class TestCommand(TestCase):
         for codename in codenames:
             p = Permission.objects.get(codename=codename)
             self.assertTrue(p in g.permissions.all())
+
+    def test_parcel_perms_to_company_manager(self):
+        call_command(self.command_name)
+        g = Group.objects.get(name="company_manager")
+
+        codenames = ["view_parcel", "add_parcel", "change_parcel"]
+        for codename in codenames:
+            p = Permission.objects.get(codename=codename)
+            self.assertTrue(p in g.permissions.all())
+
+    def test_parcel_perms_to_company_user(self):
+        call_command(self.command_name)
+        g = Group.objects.get(name="company_user")
+
+        codenames = ["view_parcel", "add_parcel", "change_parcel"]
+        for codename in codenames:
+            p = Permission.objects.get(codename=codename)
+            self.assertTrue(p in g.permissions.all())
