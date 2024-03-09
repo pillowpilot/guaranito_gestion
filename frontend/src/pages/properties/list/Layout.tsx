7e7a9eba071f3bc4c5982d5f5691436719b1839d
federico <carlos.federico005@gmail.com>
2024-03-09 16:57:45 -0300

    refactor (list properties page): extract layout

---
+++ frontend/src/pages/properties/list/Layout.tsx
@@ -0,0 +1,50 @@
+import { Box, Button, Paper, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+
+const Actions = () => {
+  const { t } = useTranslation();
+
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "row",
+        gap: 1,
+      }}
+    >
+      <Button
+        variant="contained"
+        color="primary"
+        component={Link}
+        to={`new`}
+        sx={{
+          marginTop: 2,
+        }}
+      >
+        {t("properties.list.newBtn")}
+      </Button>
+    </Box>
+  );
+};
+
+const PageLayout = ({ children }) => {
+  const { t } = useTranslation();
+
+  return (
+    <Paper
+      sx={{
+        padding: 5,
+        display: "flex",
+        flexDirection: "column",
+        gap: 2,
+      }}
+    >
+      <Typography variant="h4">{t("properties.list.header")}</Typography>
+      {children}
+      <Actions />
+    </Paper>
+  );
+};
+
+export { PageLayout };
--- frontend/src/pages/properties/list/PropertiesListPage.jsx
+++ frontend/src/pages/properties/list/PropertiesListPage.jsx
@@ -1,14 +1,6 @@
 import React, { useState } from "react";
-import {
-  Box,
-  Button,
-  Paper,
-  IconButton,
-  Typography,
-  Stack,
-} from "@mui/material";
+import { IconButton, Stack } from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete";
-import { Link } from "react-router-dom";
 import { useQueryClient, useMutation } from "react-query";
 import { useTranslation } from "react-i18next";
 import { Api } from "../../../api/client";
@@ -23,51 +15,7 @@ import { PropertiesDataGrid } from "./DataGrid";
 import { queryKeys } from "../queries";
 import { useListProperties } from "../../../hooks/property/useListProperties";
 import { useNotification } from "../../../hooks/useNotification";
-
-const PageLayout = ({ children }) => {
-  const { t } = useTranslation();
-
-  return (
-    <Paper
-      sx={{
-        padding: 5,
-        display: "flex",
-        flexDirection: "column",
-        gap: 2,
-      }}
-    >
-      <Typography variant="h4">{t("properties.list.header")}</Typography>
-      {children}
-      <Actions />
-    </Paper>
-  );
-};
-
-const Actions = () => {
-  const { t } = useTranslation();
-
-  return (
-    <Box
-      sx={{
-        display: "flex",
-        flexDirection: "row",
-        gap: 1,
-      }}
-    >
-      <Button
-        variant="contained"
-        color="primary"
-        component={Link}
-        to={`new`}
-        sx={{
-          marginTop: 2,
-        }}
-      >
-        {t("properties.list.newBtn")}
-      </Button>
-    </Box>
-  );
-};
+import { PageLayout } from "./Layout";
 
 const PropertiesPage = () => {
   const { t } = useTranslation();
