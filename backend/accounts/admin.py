056dc289d29085c0ea56ee23e132baac0e6ee013
federico <carlos.federico005@gmail.com>
2024-03-20 23:17:19 -0300

    fix (account): fix custom user admin page

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,9 +1,30 @@
+from django.utils.translation import gettext_lazy as _
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
 from accounts.models import Company, User
 
 
 @admin.register(User)
-class UserAdmin(admin.ModelAdmin): ...
+class UserAdmin(DjangoUserAdmin):
+    ordering = ["email"]
+    list_display = ["email"]
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
 
 
 @admin.register(Company)
